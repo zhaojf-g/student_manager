d5e453584eb998ac61e1af78eb22d05d7fc3f39b
赵健锋 <luckyzm2011@163.com>
2019-11-24 15:00:50 +0800

    无

---
+++ studentmanager/src/main/java/com/lon/personnelmanager/common/Result/ResultCode.java
@@ -0,0 +1,9 @@
+package com.lon.personnelmanager.common.Result;
+
+public class ResultCode {
+	
+	final public static Integer RESULT_SUCCESS_CODE = 200;
+	final public static Integer RESULT_FAIL_CODE = 201;
+	final public static Integer RESULT_ERROR_CODE = 500;
+
+}
---
+++ studentmanager/src/main/java/com/lon/personnelmanager/common/Result/ResultData.java
@@ -0,0 +1,37 @@
+package com.lon.personnelmanager.common.Result;
+
+public class ResultData {
+	
+	private Integer code = 200;
+	
+	private String message;
+	
+	private Object data;
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+	
+
+}
