6c9a26ffa09ab385852d2096e8d976a81cd1a9c1
赵健锋 <luckyzm2011@163.com>
2019-11-24 16:12:42 +0800

    1.加入swagger

--- studentmanager/src/main/java/com/lon/personnelmanager/App.java
+++ studentmanager/src/main/java/com/lon/personnelmanager/App.java
@@ -3,7 +3,10 @@ package com.lon.personnelmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class App 
 {
     public static void main( String[] args )
